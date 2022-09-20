9aa09e3085fddb0639d3519fe3d669fd117a7fb1
Choi Dahye <choi.dahye424@gmail.com>
2022-09-20 21:21:09 +0900

    스택을 사용한 코드 괄호 검사기 v1

--- code-checker.js
+++ code-checker.js
@@ -1,11 +1,9 @@
-// 코드에서 괄호와 따옴표를 추출하는 함수
+// 코드에서 괄호를 추출하는 함수
 function bracketPicker(code) {
     let codeString = code.split("");
     let brackets = [];
     for (let i = 0; i < codeString.length; i++) {
-        if (codeString[i] === '{' || codeString[i] === '}' ||
-            codeString[i] === '(' || codeString[i] === ')' ||
-            codeString[i] === '[' || codeString[i] === ']' ) {
+        if (codeString[i] === '{' || codeString[i] === '}') {
             brackets.push(codeString[i])
         }
     }
@@ -13,98 +11,42 @@ function bracketPicker(code) {
 }
 
 // 중괄호{}를 체크하는 함수
-function braceChecker(codeString, codeMap) {
-    // value에 들어갈 값을 선언한다.
-    let braceCount = 0;
-    codeMap.set('{}', 0);
+function braceChecker(codeString) {
+    // 1. stack으로 사용할 배열을 만든다.
+    let stack = [];
 
     for (let i = 0; i < codeString.length; i++) {
-        // 문자열에 '{'가 있을 경우 key가 '{}'인 value에 +1을 하고
+        // 1. 문자열에 '{'가 있을 경우 stack에 넣어준다.
         if (codeString[i] === '{') {
-            braceCount++;
-            codeMap.set('{}', braceCount);
+            stack.push('{')
         }
-        // 문자열에 '}'가 있을 경우 key가 '{}'인 value에 -1을 한다.
+        // 2. 문자열에 '}'가 있을 경우
         else if (codeString[i] === '}') {
-            braceCount--;
-            codeMap.set('{}', braceCount);
+            // 2-1. 짝이되는 괄호가 없으면 false를 반환한다.
+            if (stack.length === 0) {
+                return false;    
+            // 2-2. 짝이되는 괄호가 있으면 stack에서 꺼낸다.
+            } else {
+                stack.pop();
+            }
         } 
     }
 
-    // 문자열을 한번씩 다 체크하고 나면 key: value로 담아둔 값을 확인하고 true인지 false인지 정한다.
-    if (codeMap.get('{}') === 0) {
+    // 3. 배열을 모두 체크하고 나서 stack에 값이 없으면 true, 그 외에는 false를 반환한다.
+    if (stack.length === 0) {
         return true;
     } else {
         return false;
     }
-}
-
-// 소괄호()를 체크하는 함수
-function parenthesesChecker(codeString, codeMap) {
-    // value에 들어갈 값을 선언한다.
-    let parenthesesCount = 0;
-    codeMap.set('()', 0);
-
-    for (let i = 0; i < codeString.length; i++) {
-        // 문자열에 '('가 있을 경우 key가 '()'인 value에 +1을 하고
-        if (codeString[i] === '(') {
-            parenthesesCount++;
-            codeMap.set('()', parenthesesCount);
-        }
-        // 문자열에 ')'가 있을 경우 key가 '())'인 value에 -1을 한다.
-        else if (codeString[i] === ')') {
-            parenthesesCount--;
-            codeMap.set('()', parenthesesCount);
-        }
-    }
-
-    // 문자열을 한번씩 다 체크하고 나면 key: value로 담아둔 값을 확인하고 true인지 false인지 정한다.
-    if (codeMap.get('()') === 0) {
-        return true;
-    } else {
-        return false;
-    }    
-}
-
-// 대괄호[]를 체크하는 함수
-function bracketsChecker(codeString, codeMap) {
-    // value에 들어갈 값을 선언한다.
-    let bracketsCount = 0;
-    codeMap.set('[]', 0);
-
-    for (let i = 0; i < codeString.length; i++) {
-        // 문자열에 '['가 있을 경우 key가 '[]'인 value에 +1을 하고
-        if (codeString[i] === '[') {
-            bracketsCount++;
-            codeMap.set('[]', bracketsCount);
-        }
-        // 문자열에 ')'가 있을 경우 key가 '[]'인 value에 -1을 한다.
-        else if (codeString[i] === ']') {
-            bracketsCount--;
-            codeMap.set('[]', bracketsCount);
-        }
-    }
-
-    // 문자열을 한번씩 다 체크하고 나면 key: value로 담아둔 값을 확인하고 true인지 false인지 정한다.
-    if (codeMap.get('[]') === 0) {
-        return true;
-    } else {
-        return false;
-    }    
 }
 
 function codeChecker(code) {
-    // 1. 문자열에서 {}, ()을 담는다.
+    // 1. 문자열에서 괄호만 추출한 배열을 변수에 담는다.
     let codeString = bracketPicker(code);
 
-    // 2. key: value 형태의 자료구조를 만들어준다.
-    const codeMap = new Map();
-
-    // 3. 문자열에 {}, ()를 하나씩 짝이 맞는지 검사하여 true 혹은 false를 반환한다.
-    let isBraceOk = braceChecker(codeString, codeMap);
-    let isParenthesesOk = parenthesesChecker(codeString, codeMap);
-    let isBracketsOk = bracketsChecker(codeString, codeMap);
-    if (isBraceOk && isParenthesesOk && isBracketsOk) {
+    // 3. 문자열에 {}를 하나씩 짝이 맞는지 검사하여 true 혹은 false를 반환한다.
+    let isBraceOk = braceChecker(codeString);
+    if (isBraceOk) {
         console.log('true');
         return true;
     } else {
@@ -114,15 +56,11 @@ function codeChecker(code) {
 }
 
 // 코드 검사 함수 테스트
-let code1 = 'function(){{}}'; // true
-let code2 = 'function(){{'; // false
-let code3 = 'function()'; // true
-let code4 = 'function(()'; // false
-let code5 = 'let test = [];' // true
-let code6 = 'let test = []];' // false
+let code1 = '{{}}'; // true
+let code2 = '{{{{}}'; // false
+let code3 = '{}{}'; // true
+let code4 = '{}}{{}'; // false
 codeChecker(code1);
 codeChecker(code2);
 codeChecker(code3);
 codeChecker(code4);
-codeChecker(code5);
-codeChecker(code6);
